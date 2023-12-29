35fcf88fc80360cc07de9361c2282c7bc11bb9e8
muaaz042 <muaaz.ahmad042@gmail.com>
2023-12-29 17:23:34 +0500

    add files

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home  from './components/Home';
-import Navbar from './components/Navbar';
-import DataFetcher from './components/DataFetcher';
 
 const App = () => {
   return (
-    // <DataFetcher/>
-    <Home/>
-    // <Router>
-    //   <Navbar/>
-    //   <Routes>
-    //     <Route path='/home' element= {<Home/>}>Home</Route>
-    //     <Route path='/details' element= {<Details/>}>Home</Route>
-    //   </Routes>
-    // </Router>
+  <>
+   <Home/>
+  </>
+   
   )
 }
 
